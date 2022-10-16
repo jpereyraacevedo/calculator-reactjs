e9969b200accccdd4c58637d7e4861e0f3f45695
Jonatan Pereyra Acevedo <jpereyra@nectica.com.ar>
2022-10-16 19:11:42 -0300

    style ended

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,11 +4,12 @@ import "./Button.css"
 function Button (props) {
 
   const isOperator= value => {
-    return isNaN(value) && (value != ".") && (value != "=")
+    return isNaN(value)
   }
 
     return ( 
-      <div className={`button-container ${isOperator(props.children) ? "operator" : " "}`.trimEnd()}
+      <div 
+      className={`button-container ${isOperator(props.children) ? "operator" : " "}`.trimEnd()}
       onClick= {()=> props.handleClick(props.children)}>
         {props.children}
       </div>
